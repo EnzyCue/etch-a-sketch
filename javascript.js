4a7a61a6071666ad159b135ab9b759a65e6c7aed
EnzyCue <mohammadalhakim787@gmail.com>
2023-01-16 15:04:42 +0400

    Polish Button and centre grid, start work on implementing button.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,27 @@
 const container = document.querySelector('.container');
 
-for(let i =0; i < 16; ++i){
-    const row = document.createElement('div');
-    row.classList.add('row');
-    container.appendChild(row);
-    for(let j=0; j < 16; ++j){
-        const box = document.createElement('div');
-        box.classList.add('box');
-        box.addEventListener('mouseover', () => {
-            box.style.backgroundColor = 'grey' 
-        })
-        row.appendChild(box);
+
+function gridGenerator(dimension){
+    for(let i = 0; i < dimension; ++i){
+        const row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+        for(let j = 0; j < dimension; ++j){
+            const box = document.createElement('div');
+            box.classList.add('box');
+            box.addEventListener('mouseover', () => {
+                box.style.backgroundColor = 'grey' 
+            })
+            row.appendChild(box);
+        }
     }
 }
 
+
+cons btn = document.querySelector('button');
+
+
+btn.addEventListener('click', () => {
+    let input = prompt("How many squares per side of the new grid? :D");
+
+})
